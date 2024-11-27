c884a0ca1e7f9a0c14cbe9186ce4df4ee08890ef
Harsh Sharma <hs804506@gmail.com>
2024-11-27 23:04:25 +0530

    basic http navigations added in index.js

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const http = require("http");
 console.log("Hi")
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.end("Hello")
+    if(req.url == "/"){
+        res.end("Home Page !!");
+    }else if(req.url == "/about"){
+        res.end("About Page !!");
+    }else if(req.url == "/contact"){
+        res.end("Contact Page !!");
+    }else if(req.url == "/login"){
+        res.end("Login Page !!");
+    }
 })
 
 server.listen(4000)
